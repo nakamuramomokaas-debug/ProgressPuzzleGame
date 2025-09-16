b6f62c7915f606ec8013c6b44adc39b9a0f2eade
Pickles <39ma.rionc@gmail.com>
2025-09-17 06:44:23 +0900

    基盤作成

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings
+{
+    public enum Direction
+    {
+        Down,
+        Left,
+    }
+
+    public Direction direction;
+}
